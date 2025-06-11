dc38eae5c9663ae0d061ebadb912335a0d4ff168
ДмитроСавоста <deanwincester1907@hmail.com>
2025-06-11 08:46:14 +0300

    Оновлено метод edit_phone: додано перевірку валідності нового номера

--- HW-6.py
+++ HW-6.py
@@ -1,25 +1,3 @@
-from collections import UserDict
-
-class Field:
-    def __init__(self, value):
-        self.value = value
-
-    def __str__(self):
-        return str(self.value)
-
-class Name(Field):
-    pass
-
-class Phone(Field):
-    def __init__(self, value):
-        if not self.is_valid_phone(value):
-            raise ValueError("Phone number must be 10 digits")
-        super().__init__(value)
-
-    @staticmethod
-    def is_valid_phone(phone):
-        return phone.isdigit() and len(phone) == 10
-
 class Record:
     def __init__(self, name):
         self.name = Name(name)
@@ -38,7 +16,13 @@ class Record:
     def edit_phone(self, old_phone, new_phone):
         phone_to_edit = self.find_phone(old_phone)
         if phone_to_edit:
-            phone_to_edit.value = new_phone
+            # Спочатку перевіряємо валідність нового номера
+            if not Phone.is_valid_phone(new_phone):
+                raise ValueError("New phone number must be 10 digits")
+            # Видаляємо старий номер
+            self.remove_phone(old_phone)
+            # Додаємо новий номер
+            self.add_phone(new_phone)
         else:
             raise ValueError("Phone not found")
 
@@ -50,17 +34,3 @@ class Record:
 
     def __str__(self):
         return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
-
-class AddressBook(UserDict):
-    def add_record(self, record):
-        self.data[record.name.value] = record
-
-    def find(self, name):
-        return self.data.get(name)
-
-    def delete(self, name):
-        if name in self.data:
-            del self.data[name]
-
-    def __str__(self):
-        return "\n".join(str(record) for record in self.data.values())
